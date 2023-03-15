5279e8445fa9752bd7450455435a8cfee055efcf
Marcus Vinicius Da Silva Montanher <marcus.montanher@capgemini.com>
2023-03-15 14:35:40 -0300

    findById para chamado implementado

---
+++ src/main/java/com/marcusmontanher/helpdesk/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.marcusmontanher.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcusmontanher.helpdesk.domain.Chamado;
+import com.marcusmontanher.helpdesk.repositories.ChamadoRepository;
+import com.marcusmontanher.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+	
+}
