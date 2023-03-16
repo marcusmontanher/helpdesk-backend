85694ecf306f9065ef365af45d31de5224499121
Marcus Vinicius Da Silva Montanher <marcus.montanher@capgemini.com>
2023-03-16 16:53:39 -0300

    autenticacao com jwt

---
+++ src/main/java/com/marcusmontanher/helpdesk/domain/dtos/CredenciaisDTO.java
@@ -0,0 +1,24 @@
+package com.marcusmontanher.helpdesk.domain.dtos;
+
+public class CredenciaisDTO {
+
+	private String email;
+	private String senha;
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
